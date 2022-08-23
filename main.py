240f493796b0300d7d789f4e46d491700ec1020c
Tristan <tristan.bincaz@gmail.com>
2022-08-23 15:31:28 +0200

    Added data storing using json files. Started a GUI.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import tkinter as tk
+import json
 
 
 class Task:
@@ -13,7 +15,7 @@ class Task:
         self.complete = completed
 
     @classmethod
-    def create(self):
+    def create(cls):
         """
         Interactively creates a new task at the cli.
         """
@@ -28,10 +30,14 @@ class Task:
 
         date = datetime(year, month, day, hour)
 
-        return self(title, content, date)
+        return cls(title, content, date)
 
     def __str__(self):
-        return f"{self.title} \n{self.content}\n{self.date}"
+        return f"{self.title} \n{self.content}\n{self.date}\n{self.complete}"
+
+    def is_identical(self, task):
+        """Checks if another task is identical."""
+        return self.title == task.title and self.date == task.date
 
     def __lt__(self, task):
         return self.date < task.date
@@ -53,14 +59,24 @@ class Task:
 
 
 class ToDoList:
-    """"""
+    """List of tasks manipulation."""
 
     def __init__(self):
         self.list = list()
 
-    def add_task(self, task: Task) -> None:
-        """Adds a new task to the ToDoList."""
+    def add_task(self, task: Task) -> bool:
+        """
+        Adds a new unique task to the ToDoList.
+        Parameters :
+            task : Task object
+        Returns:
+            bool, identicating success
+        """
+        for existing_task in self.list:
+            if task.is_identical(existing_task):
+                return False
         self.list.append(task)
+        return True
 
     def late_tasks(self) -> list:
         """
@@ -80,33 +96,129 @@ class ToDoList:
 
     def upcoming_tasks(self) -> list:
         """
-        Return list of tasks:
-            - with a date attribute superior to the current date
+        Return list of tatsks:
+            - with a datate attribute superior to the current date
             - that are incomplete
         """
         return [task for task in self.list
                 if task.date > datetime.now() and not task.complete]
 
+    def store_data(self, filename: str) -> bool:
+        """Stores ToDoList in a json file."""
+        dump_list = []
+        for task in self.list:
+            dump_list.append([task.title, task.content, 
+                              datetime.strftime(task.date, "%Y-%m-%d %H:%M"),
+                              task.complete])
+        
+        print(json.dumps(dump_list))
+
+        try:
+            with open(filename, 'w') as outfile:
+                json.dump(dump_list, outfile)
+        except:
+            return False
+        
+        return True
+
+    def load_data(self, filename: str) -> bool:
+        """Load tasks from a json file."""
+
+        try:
+            with open(filename, 'r') as data_file:
+                data = json.load(data_file)
+        except:
+            return False
+        
+        for raw_task in data:
+            
+            self.list.append(
+                Task(
+                 raw_task[0],
+                 raw_task[1],
+                 datetime.strptime(raw_task[2], "%Y-%m-%d %H:%M"),
+                 raw_task[3]
+                )
+            )
+
+        return True
+
+    @classmethod
+    def instance_from_data(cls, filename: str):
+        """Creates ToDoList containing tasks from a json file."""
+        
+        with open(filename, 'r') as data_file:
+            data = json.load(data_file)
+
+        task_list = []
+        
+        for raw_task in data:
+            
+            task_list.append(
+                Task(
+                 raw_task[0],
+                 raw_task[1],
+                 datetime.strptime(raw_task[2], "%Y-%m-%d %H:%M"),
+                 raw_task[3]
+                )
+            )
+
+        todolist = cls()
+        todolist.list = task_list
+
+        return todolist
+    
     def __str__(self):
         string = ""
         for task in self.list:
             string += str(task) + "\n\n"
         return string
 
+class GUI(tk.Frame):
+    """Graphical interface for the todolist."""
 
-#task1 = Task.create()
-task1 = Task("test1", "first task", datetime(1,1,1))
-list1 = ToDoList()
-list1.add_task(task1)
-task2 = Task("test2", "second task", datetime.now(), True)
-task3 = Task("test3", "oops ! i forgor", datetime(2020, 1, 1))
-task4 = Task("test3", "signing test", datetime(2022, 2, 2))
-list1.add_task(task4)
-list1.add_task(task2)
-list1.add_task(task3)
-print(task4)
-print(task1)
-print(list1)
-print(list1.late_tasks())
-print(list1.completed_tasks())
-print(list1.upcoming_tasks())
+    def __init__(self):
+        """Constructor."""
+        self.master = tk.Tk()
+        tk.Frame.__init__(self, self.master)
+        self.pack()
+
+        self.menu = tk.Menu(self.master)
+        self.menu.add_command(label="new")
+
+        self.master.config(menu=self.menu)
+        self.master.mainloop()
+
+
+
+
+                                   
+                                   
+if __name__ == '__main__':
+    #task1 = Task.create()
+    task1 = Task("test1", "first task", datetime(1001,1,1))
+    list1 = ToDoList()
+    list1.add_task(task1)
+    task2 = Task("test2", "second task", datetime(2023, 5, 20), True)
+    task3 = Task("test3", "oops ! i forgor", datetime(2020, 1, 1))
+    task4 = Task("test4", "signing test", datetime(2022, 2, 2))
+    list1.add_task(task4)
+    list1.add_task(task2)
+    list1.add_task(task3)
+    print(task4)
+    print(task1)
+    print(list1)
+    print(list1.late_tasks())
+    print(list1.completed_tasks()[0].title)
+    print(list1.upcoming_tasks())
+    print()
+    list1.store_data("data.json")
+
+    list2 = ToDoList.instance_from_data("data.json")
+    
+    print(list2)
+
+
+    appli = GUI()
+
+    
