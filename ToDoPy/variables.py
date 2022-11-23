602fb1bda1da2b06cd0a5dbab7da1516d2821c97
Tristan <tristan.bincaz@gmail.com>
2022-11-23 14:47:42 +0100

    added documentation to variables.py

--- ToDoPy/variables.py
+++ ToDoPy/variables.py
@@ -1,4 +1,13 @@
+"""
+Used to store constant configuration variables.
+"""
+
+# File where temporary data is stored
 SAVEFILE="data.json"
+
+# Size of the main window.
 GUI_GEOMETRY = "800x400"
+
+# Size of the window that adds tasks.
 TASK_GEOMETRY = "400x200"
 
